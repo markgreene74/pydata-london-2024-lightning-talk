44333b27117ac78e4c8e5fd6c837c49739137ee7
markgreene74 <markgreene74@users.noreply.github.com>
2024-06-18 00:36:43 +0100

    second example, changed my mind

--- notebooks/example-2.py
+++ notebooks/example-2.py
@@ -16,16 +16,22 @@
 # # Example 2 - Jupyter notebook -> Python file
 
 # %% [markdown]
-# This example is taken from the Seaborn documentation (https://seaborn.pydata.org/examples/scatterplot_categorical.html)
+# This example is taken from the Seaborn documentation (https://seaborn.pydata.org/examples/regression_marginals.html)
 
 # %%
 import seaborn as sns
 
-sns.set_theme(style="whitegrid", palette="muted")
+sns.set_theme(style="darkgrid")
 
-# Load the penguins dataset
-df = sns.load_dataset("penguins")
-
-# Draw a categorical scatterplot to show each observation
-ax = sns.swarmplot(data=df, x="body_mass_g", y="sex", hue="species")
-ax.set(ylabel="")
+tips = sns.load_dataset("tips")
+g = sns.jointplot(
+    x="total_bill",
+    y="tip",
+    data=tips,
+    kind="reg",
+    truncate=False,
+    xlim=(0, 60),
+    ylim=(0, 12),
+    color="m",
+    height=7,
+)
